51cc7e1186b42874a5defa8cfc6f5672bb3ce5e2
Bartodziej <bartixboss@gmail.com>
2026-01-12 00:05:59 +0100

    Renamed "Add node" button to "New node" Made "New node" button open a window that allow the user to make choices about details of the node being added. Its layout is defined in src/main/resources/org/blakkroze/calculia/newnode/new-node-view.fxml.
    Added a controller for the main window in src/main/java/org/blakkroze/calculia/CalculiaControler.java

---
+++ src/main/java/org/blakkroze/calculia/CalculiaController.java
@@ -0,0 +1,44 @@
+package org.blakkroze.calculia.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.Modality;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+
+import java.io.IOException;
+
+public class CalculiaController {
+
+    @FXML
+    public void handleAddNode(ActionEvent event) {
+
+        Node sourceNode = (Node) event.getSource();
+        Stage sourceStage = (Stage) sourceNode.getScene().getWindow(); 
+
+        Stage addNodeWindow = new Stage();
+        addNodeWindow.setTitle("Add node");
+        addNodeWindow.initModality(Modality.APPLICATION_MODAL);
+        addNodeWindow.initOwner(sourceStage); 
+
+        System.out.println(getClass().getResource("../newnode/new-node-view.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../newnode/new-node-view.fxml"));
+
+        try {
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            addNodeWindow.setScene(scene);
+        }
+        catch (IOException e) {
+            throw new RuntimeException("CalculiaController: Could not load the FXMLs", e);
+        }
+
+
+        addNodeWindow.showAndWait();
+
+    }
+
+}
